c224d0883261ee8c9351dd3cde421690bed79324
karonwi <iyinoluwasamuel@gmail.com>
2022-02-09 01:25:56 +0100

    subject corrected

--- script.js
+++ script.js
@@ -74,7 +74,7 @@ $(document).ready(function(){
 
 function submitForm()
 {
-    const subject = document.getElementById('subject'.value);
+    const subject = document.getElementById('subject').value;
     const name = document.getElementById('name').value;
     const message = document.getElementById('message').value;
     window.open(`mailto:iyinoluwsamuel@gmail.com?subject=${subject}&body=${message}`);
